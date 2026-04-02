b1a27cd50075c1c8f874eb3c54a7febcb0249da2
Ahmed Mohamed <ahmed.mohamed4@ucalgary.ca>
2026-04-02 16:18:21 -0600

    setup MIPS CodeGenerator and simple register allocator

---
+++ codegen.py
@@ -0,0 +1,59 @@
+import sys
+from cpsc411.asttraversal import ASTTraversal
+
+class CodeGenerator(ASTTraversal):
+    def __init__(self, ast, symtab):
+        super().__init__(ast)
+        self.symtab = symtab
+        self.output = []
+        self.label_counter = 0
+        self.string_counter = 0
+
+        # a simple pool of available MIPS registers
+        self.free_registers = [f"$t{i}" for i in range(10)] + [f"$s{i}" for i in range(8)]
+
+        # map our semantic 'sym' IDs to MIPS labels
+        self.sym_to_label = {}
+        self._init_predefined_labels()
+
+    def _init_predefined_labels(self):
+        # predefined functions from the MS3 environment
+        self.sym_to_label['sym1'] = 'Lprints'
+        self.sym_to_label['sym2'] = 'Lprinti'
+        self.sym_to_label['sym3'] = 'Lprintb'
+        self.sym_to_label['sym4'] = 'Lprintc'
+        self.sym_to_label['sym5'] = 'Lgetchar'
+        self.sym_to_label['sym6'] = 'Lhalt'
+
+    def alloc_reg(self, lineno=None):
+        if not self.free_registers:
+            sys.stderr.write(f"error: expression too complicated, ran out of registers at or near line {lineno}\n")
+            sys.exit(1)
+        return self.free_registers.pop(0)
+
+    def free_reg(self, reg):
+        if reg and reg not in self.free_registers:
+            # put the register back into the pool to be reused
+            self.free_registers.append(reg)
+
+    def get_new_label(self):
+        lbl = f"L{self.label_counter}"
+        self.label_counter += 1
+        return lbl
+
+    def get_new_string_label(self):
+        lbl = f"LS{self.string_counter}"
+        self.string_counter += 1
+        return lbl
+
+    def emit(self, instr):
+        self.output.append(instr)
+
+    def generate(self):
+        # traversal will fill self.output
+        self.postorder()
+        return "\n".join(self.output) + "\n"
+
+def generate_code(ast, symtab):
+    cg = CodeGenerator(ast, symtab)
+    return cg.generate()
